9c36d3ec83cb85e0ad7b612acfd2fe3352926586
Luis Martinez <48891170+LuisFernandoMartinezSanhueza@users.noreply.github.com>
2020-06-05 02:54:46 -0400

    Numeros a Romanos completo
    ahora se implemento el echo de las "restas" para poder escribir bien cualquier numero <4000
    también se implemento validación para que no ingresen números no validos para ser transformados a Romanos

--- Numeros_a_Romanos.c
+++ Numeros_a_Romanos.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 
-char letras[]={'M','D','C','L','X','V','I'};
-int numeros[]={1000,500,100,50,10,5,1};
+char *letras[]={"M","DM","DCCC","DCC","DC","D","CD","CCC","CC","C","XC","LXXX","LXX","LX","L","XL","XXX","XX","X","IX","VIII","VII","VI","V","IV","III","II","I"};
+int numeros[]={1000,900,800,700,600,500,400,300,200,100,90,80,70,60,50,40,30,20,10,9,8,7,6,5,4,3,2,1};
  
 void main(){
 	int numero;
-	printf("\n------Ingrese el numero------\n -> ");
-	scanf("%d",&numero);
-	TransF(numero);
+	for(;;){
+		printf("\n------Ingrese el numero que desea transformar a Romanos------\n -> ");
+		scanf("%d",&numero);
+		if (numero >= 4000 || numero <=0){
+			printf("\n\n----El numero que usted a ingresado previamente no pude ser procesado----\n\n");
+		}else{
+			TransF(numero);
+		}
+	}
 }
 void TransF(int numero){
 	
 	int i;
-	for(i=0 ; i<7;){
+	for(i=0 ; i<28;){
 		if(numero >= numeros[i]){
-			printf("%c",letras[i]);
+			printf("%s",letras[i]);
 			numero -= numeros[i];
 		}
 		if (numero < numeros[i]){
